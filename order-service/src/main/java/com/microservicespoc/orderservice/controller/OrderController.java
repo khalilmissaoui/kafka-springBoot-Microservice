bb5a3263b4e2ef5b0d8840e73b1f25dcf8f7b44b
khalil <khalil.missaoui1@esprit.tn>
2022-12-29 13:16:20 +0100

    microservices working independently while producing and consuming events

---
+++ order-service/src/main/java/com/microservicespoc/orderservice/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.microservicespoc.orderservice.controller;
+
+
+import com.microservicespoc.basedomains.dto.Order;
+import com.microservicespoc.basedomains.dto.OrderEvent;
+import com.microservicespoc.orderservice.kafka.OrderProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    @Autowired
+    private OrderProducer orderProducer;
+
+
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("Success");
+        orderEvent.setMessage("order status is Successfully sent");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully ...";
+    }
+}
